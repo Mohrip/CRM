e63f6b85ab210581eca5fcc988841d627745a171
mohrip <m25srt@gmail.com>
2025-01-02 09:27:07 +0300

    mod model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,16 +1,7 @@
 package models
 
-type User struct {
-    ID       int    `json:"id"`
-    Name     string `json:"name"`
-    Email    string `json:"email"`
-    Password string `json:"password"`
-}
-
-package models
-
 type Customer struct {
-    ID    int    `json:"id"`
-    Name  string `json:"name"`
-    Email string `json:"email"`
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
