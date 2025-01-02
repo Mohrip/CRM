d1a344716f3375b3c6177e41f7d6a7cdfb331830
mohrip <m25srt@gmail.com>
2025-01-02 09:26:37 +0300

    "add models"

--- internal/models/user.go
+++ internal/models/user.go
@@ -6,3 +6,11 @@ type User struct {
     Email    string `json:"email"`
     Password string `json:"password"`
 }
+
+package models
+
+type Customer struct {
+    ID    int    `json:"id"`
+    Name  string `json:"name"`
+    Email string `json:"email"`
+}
